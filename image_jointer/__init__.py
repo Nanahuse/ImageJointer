5ff6b4671571816e1df73ce518e54865bd0d2419
Nanahuse <109294061+Nanahuse@users.noreply.github.com>
2023-06-05 21:36:29 +1200

    add copyright

--- image_jointer/__init__.py
+++ image_jointer/__init__.py
@@ -1,1 +1,7 @@
+# Copyright (c) 2023 Nanahuse
+# This software is released under the MIT License
+# https://github.com/Nanahuse/ImageJointer/blob/main/LICENSE
+
+# flake8: noqa
+
 from image_jointer.image_jointer import ImageJointer, Aline, Position, Blank
--- image_jointer/image_jointer.py
+++ image_jointer/image_jointer.py
@@ -1,8 +1,12 @@
+# Copyright (c) 2023 Nanahuse
+# This software is released under the MIT License
+# https://github.com/Nanahuse/ImageJointer/blob/main/LICENSE
+
 from __future__ import annotations
-from enum import Enum, auto
-from PIL import Image
-from dataclasses import dataclass
 from abc import ABC, abstractproperty
+from enum import Enum, auto
+from dataclasses import dataclass
+from PIL import Image
 
 
 class Aline(Enum):
