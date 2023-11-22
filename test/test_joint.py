a83c5b1d87f422d0f32545cb91a696d351f26e50
nanahuse <109294061+Nanahuse@users.noreply.github.com>
2023-11-23 00:28:41 +1300

    fix: add test (nest joint), change test directory layout

--- test/test_joint.py
+++ test/test_joint.py
@@ -13,34 +13,6 @@ def test_vector():
     assert Vector(5, 5) + Vector(6, 6) == Vector(11, 11)
 
 
-def test_joint_simple():
-    from image_jointer import JointAlign, ImageJointer
-    from PIL import Image
-    import numpy as np
-
-    red = Image.new("RGBA", (100, 100), (255, 0, 0))
-    green = Image.new("RGBA", (100, 100), (0, 255, 0))
-    blue = Image.new("RGBA", (100, 100), (0, 0, 255))
-
-    jointed = (
-        ImageJointer(red)
-        .joint(green, JointAlign.SIDE_CENTER)
-        .joint(ImageJointer(blue).joint(blue, JointAlign.SIDE_CENTER), JointAlign.UNDER_LEFT)
-    )
-    joint_img = jointed.to_image()
-    joint_img.save(IMAGE_FOLDER / "joint_simple_tmp.png")
-
-    correct_array = np.zeros((200, 200, 4), dtype=np.uint8)
-    correct_array[0:100, 0:100, 0] = 255
-    correct_array[0:100, 100:200, 1] = 255
-    correct_array[100:200, 0:200, 2] = 255
-    correct_array[:, :, 3] = 255
-    # 正解画像の保存
-    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint_simple_correct.png")
-
-    assert np.array_equal(np.asarray(joint_img), correct_array)
-
-
 def test_joint_side_top():
     from image_jointer import JointAlign, ImageJointer
     from PIL import Image
@@ -51,7 +23,7 @@ def test_joint_side_top():
 
     jointed = ImageJointer(red).joint(blue, JointAlign.SIDE_TOP)
     joint_img = jointed.to_image()
-    joint_img.save(IMAGE_FOLDER / "joint_side_top.png")
+    joint_img.save(IMAGE_FOLDER / "joint" / "side_top.png")
 
     correct_array = np.zeros((200, 200, 4), dtype=np.uint8)
     correct_array[0:100, 0:100, 0] = 255
@@ -59,7 +31,7 @@ def test_joint_side_top():
     correct_array[0:200, 100:200, 2] = 255
     correct_array[0:200, 100:200, 3] = 255
     # 正解画像の保存
-    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint_side_top_correct.png")
+    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint" / "side_top_correct.png")
 
     assert np.array_equal(np.asarray(joint_img), correct_array)
 
@@ -74,7 +46,7 @@ def test_joint_side_center():
 
     jointed = ImageJointer(red).joint(blue, JointAlign.SIDE_CENTER)
     joint_img = jointed.to_image()
-    joint_img.save(IMAGE_FOLDER / "joint_side_center.png")
+    joint_img.save(IMAGE_FOLDER / "joint" / "side_center.png")
 
     correct_array = np.zeros((200, 200, 4), dtype=np.uint8)
     correct_array[50:150, 0:100, 0] = 255
@@ -82,7 +54,7 @@ def test_joint_side_center():
     correct_array[0:200, 100:200, 2] = 255
     correct_array[0:200, 100:200, 3] = 255
     # 正解画像の保存
-    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint_side_center_correct.png")
+    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint" / "side_center_correct.png")
 
     assert np.array_equal(np.asarray(joint_img), correct_array)
 
@@ -97,7 +69,7 @@ def test_joint_side_bottom():
 
     jointed = ImageJointer(red).joint(blue, JointAlign.SIDE_BOTTOM)
     joint_img = jointed.to_image()
-    joint_img.save(IMAGE_FOLDER / "joint_side_bottom.png")
+    joint_img.save(IMAGE_FOLDER / "joint" / "side_bottom.png")
 
     correct_array = np.zeros((200, 200, 4), dtype=np.uint8)
     correct_array[100:200, 0:100, 0] = 255
@@ -105,7 +77,7 @@ def test_joint_side_bottom():
     correct_array[0:200, 100:200, 2] = 255
     correct_array[0:200, 100:200, 3] = 255
     # 正解画像の保存
-    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint_side_bottom_correct.png")
+    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint" / "side_bottom_correct.png")
 
     assert np.array_equal(np.asarray(joint_img), correct_array)
 
@@ -120,7 +92,7 @@ def test_joint_down_left():
 
     jointed = ImageJointer(red).joint(blue, JointAlign.UNDER_LEFT)
     joint_img = jointed.to_image()
-    joint_img.save(IMAGE_FOLDER / "joint_down_left.png")
+    joint_img.save(IMAGE_FOLDER / "joint" / "down_left.png")
 
     correct_array = np.zeros((200, 200, 4), dtype=np.uint8)
     correct_array[0:100, 0:100, 0] = 255
@@ -128,7 +100,7 @@ def test_joint_down_left():
     correct_array[100:200, 0:200, 2] = 255
     correct_array[100:200, 0:200, 3] = 255
     # 正解画像の保存
-    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint_down_left_correct.png")
+    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint" / "down_left_correct.png")
 
     assert np.array_equal(np.asarray(joint_img), correct_array)
 
@@ -143,7 +115,7 @@ def test_joint_down_center():
 
     jointed = ImageJointer(red).joint(blue, JointAlign.UNDER_CENTER)
     joint_img = jointed.to_image()
-    joint_img.save(IMAGE_FOLDER / "joint_down_center.png")
+    joint_img.save(IMAGE_FOLDER / "joint" / "down_center.png")
 
     correct_array = np.zeros((200, 200, 4), dtype=np.uint8)
     correct_array[0:100, 50:150, 0] = 255
@@ -151,7 +123,7 @@ def test_joint_down_center():
     correct_array[100:200, 0:200, 2] = 255
     correct_array[100:200, 0:200, 3] = 255
     # 正解画像の保存
-    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint_down_center_correct.png")
+    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint" / "down_center_correct.png")
 
     assert np.array_equal(np.asarray(joint_img), correct_array)
 
@@ -166,7 +138,7 @@ def test_joint_down_right():
 
     jointed = ImageJointer(red).joint(blue, JointAlign.UNDER_RIGHT)
     joint_img = jointed.to_image()
-    joint_img.save(IMAGE_FOLDER / "joint_down_right.png")
+    joint_img.save(IMAGE_FOLDER / "joint" / "down_right.png")
 
     correct_array = np.zeros((200, 200, 4), dtype=np.uint8)
     correct_array[0:100, 100:200, 0] = 255
@@ -174,9 +146,37 @@ def test_joint_down_right():
     correct_array[100:200, 0:200, 2] = 255
     correct_array[100:200, 0:200, 3] = 255
     # 正解画像の保存
-    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint_down_right_correct.png")
+    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint" / "down_right_correct.png")
 
     assert np.array_equal(np.asarray(joint_img), correct_array)
+
+
+def test_joint_nest():
+    from image_jointer import JointAlign, ImageJointer
+    from PIL import Image
+    import numpy as np
+
+    red = Image.new("RGBA", (100, 100), (255, 0, 0))
+    green = Image.new("RGBA", (100, 100), (0, 255, 0))
+    blue = Image.new("RGBA", (100, 100), (0, 0, 255))
+
+    nest0 = (
+        ImageJointer(red)
+        .joint(green, JointAlign.SIDE_CENTER)
+        .joint(ImageJointer(blue).joint(blue, JointAlign.SIDE_CENTER), JointAlign.UNDER_LEFT)
+    )
+    nest0_image = nest0.to_image()
+    nest0_image.save(IMAGE_FOLDER / "joint" / "nest0.png")
+
+    nest1 = (
+        ImageJointer()
+        .joint(ImageJointer(red).joint(blue, JointAlign.UNDER_CENTER), JointAlign.SIDE_CENTER)
+        .joint(ImageJointer(green).joint(blue, JointAlign.UNDER_CENTER), JointAlign.SIDE_CENTER)
+    )
+    nest1_image = nest1.to_image()
+    nest1_image.save(IMAGE_FOLDER / "joint" / "nest1.png")
+
+    assert np.array_equal(np.asarray(nest0_image), np.asarray(nest1_image))
 
 
 def test_blank():
@@ -190,13 +190,13 @@ def test_blank():
 
     jointed = ImageJointer(red).joint(blank, JointAlign.SIDE_CENTER).joint(green, JointAlign.SIDE_CENTER)
     joint_img = jointed.to_image()
-    joint_img.save(IMAGE_FOLDER / "joint_blank_tmp.png")
+    joint_img.save(IMAGE_FOLDER / "blank" / "blank_tmp.png")
 
     correct_array = np.zeros((100, 250, 4), dtype=np.uint8)
     correct_array[0:100, 0:100, 0] = 255
     correct_array[0:100, 0:100, 3] = 255
     correct_array[0:100, 150:250, 1] = 255
     correct_array[0:100, 150:250, 3] = 255
-    Image.fromarray(correct_array).save(IMAGE_FOLDER / "joint_blank_correct.png")
+    Image.fromarray(correct_array).save(IMAGE_FOLDER / "blank" / "blank_correct.png")
 
     assert np.array_equal(np.asarray(joint_img), correct_array)
