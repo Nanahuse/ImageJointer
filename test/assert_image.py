0136d087a74e3f02357e11b8f49fcd657049e2b3
nanahuse <109294061+Nanahuse@users.noreply.github.com>
2023-12-24 02:55:42 +1300

    fix assert_image

--- test/assert_image.py
+++ test/assert_image.py
@@ -1,7 +1,5 @@
-from PIL import Image, ImageChops
+from PIL import Image
 
 
-def assert_image(image: Image.Image, expected_image: Image.Image):
-    diff = ImageChops.difference(image, expected_image)
-
-    assert diff.getbbox() is None
+def assert_image(image: Image.Image, expected_image: Image.Image, is_same: bool = True):
+    assert (image.tobytes() == expected_image.tobytes()) == is_same
